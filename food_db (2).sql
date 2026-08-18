b589e6ea6943b30c056953e8e80f1cf9bf2dc694
JamGarcia06 <2092617@g.cu.edu.ph>
2026-08-18 20:42:27 +0800

    Add files via upload

---
+++ food_db (2).sql
@@ -0,0 +1,186 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 18, 2026 at 02:42 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `food_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_acc`
+--
+
+CREATE TABLE `admin_acc` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` enum('admin') NOT NULL DEFAULT 'admin',
+  `store_name` varchar(100) NOT NULL DEFAULT '''My Food Store''',
+  `store_image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin_acc`
+--
+
+INSERT INTO `admin_acc` (`id`, `name`, `email`, `password`, `role`, `store_name`, `store_image`) VALUES
+(1, 'Administrator', 'admin@gmail.com', 'admin123', 'admin', '\'Pera Pera store\'', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foods`
+--
+
+CREATE TABLE `foods` (
+  `id` int(11) NOT NULL,
+  `food_name` varchar(100) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `status` enum('available','unavailable') NOT NULL DEFAULT 'available'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `foods`
+--
+
+INSERT INTO `foods` (`id`, `food_name`, `description`, `price`, `quantity`, `status`) VALUES
+(2, 'Adobo', 'yeah', '50.00', 0, 'available'),
+(3, 'lechon', 'shesh', '50.00', 1, 'unavailable'),
+(4, 'kinilaw', 'isda', '50.00', 10, 'available'),
+(5, 'Fried Chicken', 'chicken pero Fried', '50.00', 12, 'available');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `food_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `reservation_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` enum('Pending','Approved','Reservation') NOT NULL DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `user_id`, `food_id`, `quantity`, `reservation_time`, `status`) VALUES
+(1, 123, 2, 1, '2026-08-17 09:48:36', 'Pending'),
+(2, 123, 2, 1, '2026-08-17 09:52:01', 'Pending'),
+(3, 123, 2, 1, '2026-08-17 09:52:31', 'Pending'),
+(4, 1, 2, 1, '2026-08-17 12:55:12', ''),
+(5, 1, 2, 1, '2026-08-17 12:57:23', ''),
+(6, 1, 2, 3, '2026-08-17 10:15:00', 'Pending'),
+(7, 1, 2, 1, '2026-08-17 10:28:27', 'Pending'),
+(8, 1, 2, 1, '2026-08-17 13:18:15', 'Approved'),
+(9, 1, 2, 1, '2026-08-17 05:28:31', 'Pending'),
+(10, 1, 2, 52, '2026-08-17 06:25:13', 'Pending'),
+(11, 1, 2, 34, '2026-08-17 06:59:55', 'Pending'),
+(12, 1, 4, 2, '2026-08-17 13:17:15', 'Approved'),
+(13, 1, 5, 5, '2026-08-18 06:22:15', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `role` enum('user') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`) VALUES
+(1, 'User', 'user@gmail.com', 'user123', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `foods`
+--
+ALTER TABLE `foods`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_acc`
+--
+ALTER TABLE `admin_acc`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `foods`
+--
+ALTER TABLE `foods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
